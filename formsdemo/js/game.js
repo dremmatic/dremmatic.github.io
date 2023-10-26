f7555965faf9e2763c7c70b710586ace744771a0
dremmatic <ewang@eastsideprep.org>
2023-10-26 15:26:37 -0700

    uploading 2truthsandalieeee

--- formsdemo/js/game.js
+++ formsdemo/js/game.js
@@ -1,5 +1,3 @@
-//this is for truths and lies
-
 //global variable
 let fname = "";
 
@@ -9,11 +7,11 @@ function greet() {
 
     //get values from the form
     fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value
-    let age = document.getElementById("age").value
+    let lname = document.getElementById("lname").value;
+    let age = document.getElementById("age").value;
 
     //show the result
-    geetParagraph.innerHTML = "Greetings " + age + " years old " + fname + " " + lname + "!"
+    geetParagraph.innerHTML = "Greetings " + age + " year old " + fname + " " + lname + "!";
 
 }
 
@@ -40,23 +38,53 @@ function trivia() {
 
 }
 
-function checkbox() {
+
+
+//2 truths and a lieee
+function truth() {
     //get handle to the result paragraph
-    let checkboxSelected = document.getElementById("trivia-answer");
+    let truthAnswer = document.getElementById("truth-answer");
 
     //result of selected trivia asnwer
-    let chocolateSelected = document.getElementById("chocolate").checked;
+    let dogSelected = document.getElementById("dog").checked;
 
-    let tunaSelected = document.getElementById("tuna").checked;
+    let superstitiousSelected = document.getElementById("superstitious").checked;
 
-    let honeySelected = document.getElementById("honey").checked;
+    let picklesSelected = document.getElementById("pickles").checked;
 
-    if (chocolateSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong. ur silly.";
-    } else if (tunaSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong. tuna is stinky";
+    if (picklesSelected) {
+        truthAnswer.innerHTML = fname + ", its true, i luv pickles";
+    } else if (superstitiousSelected) {
+        truthAnswer.innerHTML = fname + ", its true, i'm pretty superstitious";
     }
     else {
-        triviaAnswer.innerHTML = fname + ", correct! go off.";
+        truthAnswer.innerHTML = fname + ", u got me! i don't have a dog.";
     }
+
 }
+
+
+///two truths and lie but check the two truths
+function truth2() {
+    //get handle to the result paragraph
+    let truth2answer = document.getElementById("truth2-answer");
+
+    //result of selected trivia asnwer
+    let taylorSelected = document.getElementById("taylor").checked;
+
+    let ticketsSelected = document.getElementById("tickets").checked;
+
+    let hellokittySelected = document.getElementById("hellokitty").checked;
+
+    if (ticketsSelected && hellokittySelected && !taylorSelected) {
+
+        truth2answer.innerHTML = fname + ", you are correct!";
+
+    } else if (taylorSelected && ticketsSelected && !hellokittySelected) {
+        truth2answer.innerHTML = fname + ", only of these is true! i don't love taylor";
+    } else {
+        truth2answer.innerHTML = fname + ", try again!";
+    }
+
+}
+
